f8758595d1ce82a7045f38051dfff173df692cf9
chrisliiii <38777606+chrisliiii@users.noreply.github.com>
2018-05-10 11:24:27 +1000

    Update crawl.sh

--- CCC2018-Harvester/crawl.sh
+++ CCC2018-Harvester/crawl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 stream_tweets.py > stream.json
-python3 search_tweets.py stream.json search.json
+python3 stream_tweets.py > "stream.json"
+python3 search_tweets.py "stream.json" "search.json"
