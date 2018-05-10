7a7d8f11648dafd37d9a8e0b43f4a417c90b0795
chrisliiii <38777606+chrisliiii@users.noreply.github.com>
2018-05-10 11:26:07 +1000

    Update search_tweets.py

--- CCC2018-Harvester/search_tweets.py
+++ CCC2018-Harvester/search_tweets.py
@@ -18,10 +18,10 @@ def get_userid(line, ):
 
 
 # Consumer keys and access tokens to access twitter API
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_token = 	"987560529987170304-jHfyqWWMcTIDeQU9pOo5MLHGB8FRBew"
+access_token_secret = "oSczVKG3iqJJcPkiFrFfvyD2bZjHqF3DxxIpvbXxIO7UD"
+consumer_key = "zGtzvalMg8wwz5hV01ByOr7VN"
+consumer_secret = "6OoIRgFqwUU9ufSB4X2vntKiQD7Xuv49APr4GznopT5RCdEFhN"
 
 
 if __name__ == "__main__":
